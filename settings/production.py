d8886434a6c411778e9c895ad7432b76fe68916d
pf-github-pl <pf.github@gmail.com>
2022-11-29 13:15:07 +0100

    change default localhost for production settings

--- settings/production.py
+++ settings/production.py
@@ -7,7 +7,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
-    "DJANGO_ALLOWED_HOSTS", default=["timecashier.pl"]
+    "DJANGO_ALLOWED_HOSTS", default=["localhost"]
 )
 
 # DATABASES
