26c123e48798e0aa08baa0b9d2f7d7eb8ffc7a30
pf-github-pl <pf.github@gmail.com>
2022-12-05 13:26:55 +0100

    add organization models to admin

--- main/admin.py
+++ main/admin.py
@@ -1,13 +1,42 @@
 from django.contrib import admin
 
-from main.models import UserProfile
-
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
+from main.models import Organization
+from main.models import OrganizationAddress
+from main.models import OrganizationUser
+from main.models import OrganizationUserRole
 
 
 admin.site.site_header = "TimeCashier Admin"
 admin.site.site_title = "TimeCashier Admin Portal"
 admin.site.index_title = "Witaj w portalu TimeCashier"
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(OrganizationAddress)
+class OrganizationAddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "street",
+        "number",
+        "zip_code",
+        "city",
+        "organization",
+    )
+    list_filter = ("organization",)
+
+
+@admin.register(OrganizationUser)
+class OrganizationUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "organization", "user")
+    list_filter = ("organization", "user")
+
+
+@admin.register(OrganizationUserRole)
+class OrganizationUserRoleAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "role")
+    list_filter = ("user",)
