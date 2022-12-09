98789532f9020d5ff51983c10a2a5a7f3f9cfd70
pf-github-pl <pf.github@gmail.com>
2022-12-09 01:50:27 +0100

    add templates for account operations, override allauth forms

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,9 @@
+from allauth.account.forms import ChangePasswordForm
 from allauth.account.forms import LoginForm
+from allauth.account.forms import ResetPasswordForm
+from allauth.account.forms import ResetPasswordKeyForm
+from allauth.account.forms import SetPasswordForm
+from allauth.account.forms import SignupForm
 from crispy_bootstrap5.bootstrap5 import FloatingField
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Div
@@ -14,11 +19,103 @@ class LoginForm(LoginForm):
         self.helper.layout = Layout(
             Div(
                 FloatingField("login"),
-                FloatingField("password", placeholder="Enter Password"),
-                Field("remember", placeholder="Enter Password"),
+                FloatingField("password"),
+                Field("remember"),
                 Submit(
                     "submit",
                     "Zaloguj",
+                    css_class="btn-time-primary rounded-pill w-100",
+                    style="margin-top: 10px; margin-bottom: 10px;",
+                ),
+            )
+        )
+
+
+class ChangePasswordForm(ChangePasswordForm):
+    def __init__(self, *args, **kwargs):
+        super(ChangePasswordForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                FloatingField("oldpassword"),
+                FloatingField("password1"),
+                FloatingField("password2"),
+                Submit(
+                    "submit",
+                    "Zmień hasło",
+                    css_class="btn-time-primary rounded-pill w-100",
+                    style="margin-top: 10px; margin-bottom: 10px;",
+                ),
+            )
+        )
+
+
+class SignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                FloatingField("email"),
+                FloatingField("username"),
+                FloatingField("password1"),
+                FloatingField("password2"),
+                Submit(
+                    "submit",
+                    "Zarejestruj",
+                    css_class="btn-time-primary rounded-pill w-100",
+                    style="margin-top: 10px; margin-bottom: 10px;",
+                ),
+            )
+        )
+
+
+class ResetPasswordForm(ResetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super(ResetPasswordForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                FloatingField("email"),
+                Submit(
+                    "submit",
+                    "Zresetuj moje hasło",
+                    css_class="btn-time-primary rounded-pill w-100",
+                    style="margin-top: 10px; margin-bottom: 10px;",
+                ),
+            )
+        )
+
+
+class ResetPasswordKeyForm(ResetPasswordKeyForm):
+    def __init__(self, *args, **kwargs):
+        super(ResetPasswordKeyForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                FloatingField("password1"),
+                FloatingField("password2"),
+                Submit(
+                    "submit",
+                    "Zmień hasło",
+                    css_class="btn-time-primary rounded-pill w-100",
+                    style="margin-top: 10px; margin-bottom: 10px;",
+                ),
+            )
+        )
+
+
+class SetPasswordForm(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super(SetPasswordForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                FloatingField("password1"),
+                FloatingField("password2"),
+                Submit(
+                    "submit",
+                    "Ustaw hasło",
                     css_class="btn-time-primary rounded-pill w-100",
                     style="margin-top: 10px; margin-bottom: 10px;",
                 ),
--- settings/base.py
+++ settings/base.py
@@ -251,11 +251,11 @@ SOCIALACCOUNT_PROVIDERS = {
 
 ACCOUNT_FORMS = {
     "login": "main.forms.LoginForm",
-    # 'signup': 'allauth.account.forms.SignupForm',
+    "signup": "main.forms.SignupForm",
     # 'add_email': 'allauth.account.forms.AddEmailForm',
-    # 'change_password': 'allauth.account.forms.ChangePasswordForm',
-    # 'set_password': 'allauth.account.forms.SetPasswordForm',
-    # 'reset_password': 'allauth.account.forms.ResetPasswordForm',
-    # 'reset_password_from_key': 'allauth.account.forms.ResetPasswordKeyForm',
+    "change_password": "main.forms.ChangePasswordForm",
+    "set_password": "main.forms.SetPasswordForm",
+    "reset_password": "main.forms.ResetPasswordForm",
+    "reset_password_from_key": "main.forms.ResetPasswordKeyForm",
     # 'disconnect': 'allauth.socialaccount.forms.DisconnectForm',
 }
