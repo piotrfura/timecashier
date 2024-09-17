7c02520e40a1c8b8dbf1941d65a0f17ef23c2c40
pf-github-pl <pf.github@gmail.com>
2024-09-17 13:15:50 +0200

    add smtp settings into production settings

--- settings/production.py
+++ settings/production.py
@@ -19,6 +19,14 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # no
 # Django Admin URL regex.
 ADMIN_URL = env("DJANGO_ADMIN_URL")
 
+# Root directory for static and media files
+# ------------------------------------------------------------------------------
 STATIC_ROOT = env("STATIC_ROOT")
-
 MEDIA_ROOT = env("MEDIA_ROOT")
+
+# Email smtp settings
+# ------------------------------------------------------------------------------
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
